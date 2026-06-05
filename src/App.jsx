04c9012ee30a53729f4379053dfc04aa73a1df6c
ShubhPandey-dev <shubhpandey0308@gmail.com>
2026-06-05 13:14:26 +0530

    updates sidebar issues

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Navigate, Route, Routes } from "react-router-dom";
-import Navbar from "./components/Navbar";
+import Navbar from "./components/navbar";
 import Sidebar from "./components/sidebar";
 import Customers from "./Pages/Customers";
 import Dashboard from "./pages/Dashboard";
